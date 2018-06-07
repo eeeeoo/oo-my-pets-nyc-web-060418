48afee25e1a251b751d1caa7d8e4a25ad6f14079
Jee Yoon Lee <jlee66@wellesley.edu>
2018-06-07 19:59:35 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -73,13 +73,15 @@ class Owner
   end
 
   def sell_pets
-    @pets.each do |pet_type|
-      pet_type.each do |pet|
-        pet.mood = "nervous"
-      end
-    end
-    @pets.reset_all
+    # @pets.each do |pet_type|
+    #   pet_type.each do |pet|
+    #     pet.mood = "nervous"
+    #   end
+    # end
+    # @pets.reset_all
+    @pets
+    binding.pry
   end
   # binding.pry
-  
+
 end
